32aa4342059319a4800918660f079f39fd1f823d
khamraevs <khamrayevbek96@gmail.com>
2025-02-17 13:59:38 +0500

    17.02.2025
    from qo'shildi

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import db
 app = Flask(__name__)
 @app.route("/")
@@ -14,6 +14,19 @@ def about_page():
 def info_cars():
     import db
     return render_template("cars.html", cars=db.cars)
+
+@app.route("/new_car", methods=["GET", "POST"])
+def new_car():
+    if request.method == "GET":
+        return render_template("new_car.html")
+    elif request.method == "POST":
+        company = request.form.get("company")
+        model = request.form.get("model")
+        info = request.form.get("info")
+        logo = request.form.get("logo")
+        image = request.form.get("image")
+
+        return f"{company}"
 
 app.run(debug=True)
 
